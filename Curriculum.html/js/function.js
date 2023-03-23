1965fefaf94a8ee8bc75da1c91d14fee114ed4ab
gaby10alfa <126513054+gaby10alfa@users.noreply.github.com>
2023-03-23 08:21:42 -0300

    initial commit

---
+++ Curriculum.html/js/function.js
@@ -0,0 +1,26 @@
+
+document.getElementById("boton").onclick=function(){
+ console.log("capturamos elemento click"); 
+document.getElementById("demo").innerHTML="0343-4111111";
+}
+ 
+document.getElementById("boton1").onclick=function(){  
+    document.getElementById("demo1").innerHTML= "Elisa Daniels";
+   }
+
+
+document.getElementById("boton2").onclick=function(){
+      document.getElementById("demo2").innerHTML= "El Viraro 1293";
+    }
+
+
+document.getElementById("boton3").onclick=function(){
+       document.getElementById("demo3").innerHTML= "10 de Enero de 1998 .Paraná, Entre Rios ";
+    }
+
+
+document.getElementById("boton4").onclick=function(){
+      document.getElementById("demo4").innerHTML= "B1 Vehiculo propio ";
+     }
+ 
+
